2141c977d59047a36e4b105c224e0cdafec82f0c
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-05-28 13:02:24 -0400

    get rebase working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as http from "http";
 import { Server } from "socket.io";
 import { ChangeSet, Text } from "@codemirror/state";
+import { rebaseUpdates } from "@codemirror/collab";
 import * as fs from "fs";
 import * as path from "path";
 import { fileURLToPath } from "url";
@@ -77,29 +78,28 @@ io.on("connection", (socket) => {
       let { updates, pending, doc } = getDocument(documentName);
       docUpdates = JSON.parse(docUpdates);
 
-      if (version != updates.length) {
-        socket.emit("pushUpdateResponse", false);
-      } else {
-        for (let update of docUpdates) {
-          // Convert the JSON representation to an actual ChangeSet
-          // instance
-          let changes = ChangeSet.fromJSON(update.changes);
-          updates.push({
-            changes,
-            clientID: update.clientID,
-            effects: update.effects,
-          });
-          documents.set(documentName, { updates, pending, doc });
-          doc = changes.apply(doc);
-          documents.set(documentName, { updates, pending, doc });
-        }
-        socket.emit("pushUpdateResponse", true);
+      docUpdates = docUpdates.map((update) => ({
+        changes: ChangeSet.fromJSON(update.changes),
+        clientID: update.clientID,
+        effects: update.effects,
+      }));
 
-        while (pending.length) {
-          pending.pop()(updates);
-        }
+      if (version != updates.length) {
+        docUpdates = rebaseUpdates(docUpdates, updates.slice(data.version));
+      }
+      for (let update of docUpdates) {
+        let changes = update.changes;
+        updates.push(update);
+        documents.set(documentName, { updates, pending, doc }); 
+        doc = changes.apply(doc);
         documents.set(documentName, { updates, pending, doc });
       }
+      socket.emit("pushUpdateResponse", true);
+
+      while (pending.length) {
+        pending.pop()(updates); // what is this?!
+      }
+      documents.set(documentName, { updates, pending, doc });
     } catch (error) {
       console.error("pushUpdates", error);
     }
